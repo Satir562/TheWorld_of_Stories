5aed605618430f2c71162518708920db18e20fa3
Алексей Белозор <112486821+Satir562@users.noreply.github.com>
2024-03-19 08:06:48 +0300

    Update settings.py

--- utr/settings.py
+++ utr/settings.py
@@ -21,14 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mxy+-5gayds3*ol6x!^oajyb7429w*5^!=q&r1rj#05**8$e39'
+#SECRET_KEY = 'django-insecure-mxy+-5gayds3*ol6x!^oajyb7429w*5^!=q&r1rj#05**8$e39'
 import os
 
-# SECRET_KEY = os.environ.get('django-insecure-mxy+-5gayds3*ol6x!^oajyb7429w*5^!=q&r1rj#05**8$e39')
+SECRET_KEY = os.environ.get('django-insecure-mxy+-5gayds3*ol6x!^oajyb7429w*5^!=q&r1rj#05**8$e39')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+#DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+ALLOWED_HOSTS = [
+    'www.Alexey69.pythonanywhere.com',
+    'Alexey69.pythonanywhere.com',
+]
 
 # Application definition
 
